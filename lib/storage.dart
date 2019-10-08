6f2101d09c2f3913f9a55395fcf8dc135c5b3a1d
Darek-Mac <darek.baranczuk@gmail.com>
2019-10-08 17:31:09 +0200

    Added storage

---
+++ lib/storage.dart
@@ -0,0 +1,18 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Storage {
+
+  String _pinKey = "pin_";
+
+  Future savePin(String pin) async {
+    var preferences = await _providePreferences();
+    await preferences.setString(this._pinKey, pin);
+  }
+
+  Future<String> readPin() async {
+    var preferences = await _providePreferences();
+    return preferences.getString(this._pinKey);
+  }
+  
+  Future<SharedPreferences> _providePreferences() async => await SharedPreferences.getInstance();
+}
